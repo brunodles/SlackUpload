279820082a17ed40e1d34069b97fd8cd7edd9006
Bruno de Lima <dlimaun@gmail.com>
2016-10-11 15:51:05 -0300

    Update uploadTask to use slackPluginExtension

--- src/main/groovy/com/github/brunodles/slackupload/UploadTask.groovy
+++ src/main/groovy/com/github/brunodles/slackupload/UploadTask.groovy
@@ -4,6 +4,8 @@ import com.github.brunodles.util.WebClient
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+import static SlackUpload.SLACK_UPLOAD_EXTENSION
+
 /**
  * Created by bruno on 30/09/16.
  */
@@ -11,6 +13,7 @@ public class UploadTask extends DefaultTask {
 
     public static final String SLACK_UPLOAD_URL = "https://slack.com/api/files.upload"
     String token
+    String tokenFile
     String file
     String content
     String fileType
@@ -19,33 +22,75 @@ public class UploadTask extends DefaultTask {
     String initial_comment
     String channels
 
+    private ArrayList<String> errors = new ArrayList<>()
+
     @TaskAction
+    def trySendToSlack() {
+        loadFromParameters()
+        try {
+            sendToSlack()
+        } catch (Exception e) {
+            e.printStackTrace(System.err)
+        }
+    }
+
+    def loadFromParameters() {
+        if (!project.extensions.getByName(SLACK_UPLOAD_EXTENSION)) return
+        def slackExtension = project.slackUpload
+        if (!token) token = slackExtension.token
+        if (!tokenFile) tokenFile = slackExtension.tokenFile
+    }
+
     def sendToSlack() {
-        def file = null
-        if (this.file) {
-            file = new File(this.file)
-            if (!filename)
-                filename = file.getName()
+        validParameters()
+        if (!errors.isEmpty()) {
+            errors.each { s -> println s }
+            return
         }
 
         def client = new WebClient(SLACK_UPLOAD_URL)
                 .setLogLevel(WebClient.LOG_BODY)
                 .setContentTypeMultipartFormData()
                 .addPostParameter("token", token)
-                .addPostParameter("filename", filename)
-        if (file != null)
+        if (file) {
+            def file = new File(this.file)
+            if (!filename) filename = file.getName()
             client.addFileParameter("file", file)
-        else if (content)
+        } else if (content) {
             client.addPostParameter("content", content)
-        if (fileType)
-            client.addPostParameter("filetype", fileType)
-        if (title)
-            client.addPostParameter("title", title)
-        if (initial_comment)
-            client.addPostParameter("initial_comment", initial_comment)
-        if (channels)
-            client.addPostParameter("channels", channels)
+        }
+        client.addPostParameter("filename", filename)
+        if (fileType) client.addPostParameter("filetype", fileType)
+        if (title) client.addPostParameter("title", title)
+        if (initial_comment) client.addPostParameter("initial_comment", initial_comment)
+        if (channels) client.addPostParameter("channels", channels)
         def response = client.doPost()
         println "Status $response.code = $response.response"
+    }
+
+    def validParameters() {
+        validateToken()
+        validateFileName()
+    }
+
+    def validateToken() {
+        if (token != null) return true
+        if (tokenFile == null) {
+            errors.add "The 'token' and the 'tokenFile' are empty, use one of those."
+            return
+        }
+
+        def tokenFile = project.file(this.tokenFile)
+        if (tokenFile.exists()) {
+            token = tokenFile.text.trim()
+        } else {
+            println "Token file does not exist, we will create it for you."
+            tokenFile.write("")
+        }
+    }
+
+    def validateFileName() {
+        if (file || content) return
+        errors.add("Use 'file' or 'content' to upload something to slack.")
     }
 }
