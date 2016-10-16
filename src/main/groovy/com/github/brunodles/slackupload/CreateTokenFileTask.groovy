14bc4e0f7a2cb663ebca7a65aeefcb92e8445f05
Bruno de Lima <dlimaun@gmail.com>
2016-10-16 18:57:42 -0300

    Update CreateTokenTask to get input from console

--- src/main/groovy/com/github/brunodles/slackupload/CreateTokenFileTask.groovy
+++ src/main/groovy/com/github/brunodles/slackupload/CreateTokenFileTask.groovy
@@ -10,13 +10,48 @@ class CreateTokenFileTask extends DefaultTask {
 
     @TaskAction
     def createFile() {
-        def slackExtension = tryGetExtension(project, SLACK_UPLOAD_EXTENSION)
-        if (!slackExtension) {
+        def slackExtension = getSlackExtension()
+        if (!slackExtension) return
+        def tokenFile = project.file(slackExtension.tokenFile)
+        if (!existis(tokenFile)) return
+        String token = getToken()
+        writeToken(token, tokenFile)
+    }
+
+    def getSlackExtension() {
+        def extension = tryGetExtension(project, SLACK_UPLOAD_EXTENSION)
+        if (!extension) {
             println "Can't find 'slackUpload' settings"
-            return
+            return null
         }
-        def tokenFile = slackExtension.tokenFile
-        project.file(tokenFile).write("")
+        return extension;
+    }
+
+    private boolean existis(File tokenFile) {
+        if (tokenFile.exists()) {
+            println 'Token file already exists!'
+            return false
+        }
+        return true
+    }
+
+    private String getToken() {
+        String token = ""
+        def console = System.console()
         println "Create a test token on slack\nhttps://api.slack.com/docs/oauth-test-tokens"
+        if (console) {
+            token = console.readLine('> Please enter your token: ')
+        } else {
+            println "Cannot get console, you're probably running the task on a daemon.\nCreating empty file."
+        }
+        token
+    }
+
+    private void writeToken(String token, File tokenFile) {
+        if (token)
+            token = token.trim()
+        else
+            token = ""
+        tokenFile.write(token)
     }
 }
