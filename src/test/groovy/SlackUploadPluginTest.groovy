a0c04506b2ed3aa1daf8c7e0bed8e15129620322
Bruno de Lima <dlimaun@gmail.com>
2016-10-16 17:41:40 -0300

    Create test for SlackUploadPlugin

---
+++ src/test/groovy/SlackUploadPluginTest.groovy
@@ -0,0 +1,37 @@
+import com.github.brunodles.slackupload.CreateTokenFileTask
+import com.github.brunodles.slackupload.SlackUploadPluginExtension
+import com.github.brunodles.slackupload.UploadTask
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
+import org.junit.Test
+
+import static junit.framework.TestCase.assertTrue
+
+/**
+ * Created by bruno on 16/10/16.
+ */
+class SlackUploadPluginTest {
+    private Project project
+
+    @Before
+    public void applyGradlePlugin() {
+        project = ProjectBuilder.builder().build()
+        this.project.pluginManager.apply 'com.github.brunodles.SlackUpload'
+    }
+
+    @Test
+    public void shouldHave_createTokenFileTask() {
+        assertTrue(project.tasks.createTokenFile instanceof CreateTokenFileTask)
+    }
+
+    @Test
+    public void shouldHave_UploadTask() {
+        assertTrue(project.tasks.uploadTask instanceof UploadTask)
+    }
+
+    @Test
+    public void shouldHave_SlackUploadPluginExtension() {
+        assertTrue(project.extensions.slackUpload instanceof SlackUploadPluginExtension)
+    }
+}
